a017dd97641f3dbd39c3fd60d0a458901fc3cba2
Ulmasson <ulmassonme@gmail.com>
2022-12-08 16:16:42 +0500

    misol3

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,6 +1,8 @@
 #Create a variable called 'number' and assign it the two-digit number
 number=32
 #Find the reverse of the number and assign it to a variable called 'answer'.
-answer=number%10
+xona1=number%10
+xona2=number//10
+answer=xona1*10+xona2
 #Print the answer variable
 print(answer)
